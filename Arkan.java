f2680e2405fa3ec409a0be4cb51104be0c876ca6
Iqbal Rizqi <rama.ramajr121@gmail.com>
2024-07-31 11:38:27 +0700

    First Pull

---
+++ Arkan.java
@@ -0,0 +1,17 @@
+public class Arkan {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Apakah Jokowi itu Baik?");
+
+        // String jokowi = input.nextLine();
+
+        // if (jokowi == "Baik") {
+        //     System.out.println("Jokowi baik");
+        // } else {
+        //     System.out.println("Jokowi Jahat");
+        // }
+        String nama = "Iqbal Rizqi Ramadhan";
+        String kelas = "X RPL 5";
+        System.out.println("Halo saya, " + nama + " dari kelas " + kelas);
+    }
+}
